e374d18be78f28ff18ad0daa317149400aa59633
AnalystYuchels <uche4xploit@gmail.com>
2026-01-22 17:07:29 +0100

    Add in-memory data store for posts

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let posts = [];
+let idCounter = 1;
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
